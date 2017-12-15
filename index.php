4a152f61bff04d45da20a1875c107dac7a2db28a
romazio <34558486+romazio@users.noreply.github.com>
2017-12-15 19:08:14 +0000

    Add files via upload
    landing page end

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 $nomeErr = $emailErr = $passwordErr = $password_confirmErr = "";
 $nome = $email = $password = $password_confirm = "";
@@ -33,17 +32,19 @@ foreach($resultados as $linha){
 
 
 
-<!doctype html>
-<html>
-<head>
-  <title>VYNIL STORE</title>
-  <meta charset="utf-8">
-   <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+    <!doctype html>
+    <html>
 
-<body>
-  <?php
+    <head>
+        <title>VYNIL STORE</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <?php
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["nome"])) {
       $nomeErr = "nome é um campo obrigatório";
@@ -86,34 +87,145 @@ foreach($resultados as $linha){
 
 
   ?>
-  <h1>VYNIL STORE </h1>
-  <img src="img/vynil.png" id="vynil">
-  
-  <h2><?php echo $nome;?></h2>
-  <h2>LOG IN</h2>
-  <form method="post">
-    email: <input type="email" placeholder="email" name="email_login"><br>
-    password: <input type="password" name="password_login"><br>
-    <input type="submit" name="submit_login" value="L O G I N"><br>
+            <h1>VYNIL STORE </h1>
+            <img src="img/vynil.png" id="vynil">
 
-  </form>
+            <h2>
+                <?php echo $nome;?>
+            </h2>
 
-  <h2>SIGN UP</h2>
-  <form method="post">
-    nome: <input type='text' name="nome">
-    <span class="error"> <?php echo $nomeErr;?></span><br>
-    email: <input type="email" name="email">
-    <span class="error"> <?php echo $emailErr;?></span><br>
-    password: <input type="password" name="password">
-    <span class="error"> <?php echo $passwordErr;?></span><br>
-    confirm password: <input type="password" name="password_confirm">
-    <span class="error"> <?php echo $password_confirmErr;?></span><br>
-    <input type="submit" name="submit" value="C O N F I R M"><br>
+            <div id="logb">
+                <h2>LOG IN</h2>
+            </div>
+            <div id="logbg"></div>
+            <div id="log">
+                <form method="post">
+                    email: <input type="email" placeholder="email" name="email_login"><br><br> password: <input type="password" name="password_login"><br><br>
+                    <input type="submit" name="submit_login" value="L O G I N"><br>
 
-  </form>
+                </form>
+            </div>
 
+            <div id="signb">
+                <h2>SIGN UP</h2>
+            </div>
+            <div id="signbg"></div>
+            <div id="sign">
+                <form method="post">
+                    nome: <input type='text' name="nome">
+                    <span class="error"> <?php echo $nomeErr;?></span><br><br> email: <input type="email" name="email">
+                    <span class="error"> <?php echo $emailErr;?></span><br><br> password: <input type="password" name="password">
+                    <span class="error"> <?php echo $passwordErr;?></span><br> confirm password: <input type="password" name="password_confirm">
+                    <span class="error"> <?php echo $password_confirmErr;?></span><br><br>
+                    <input type="submit" name="submit" value="C O N F I R M"><br>
 
+                </form>
+            </div>
 
-</body>
+            <div id="cntb">
+                <h2>CONTACT</h2>
+            </div>
+            <div id="cnt">
+                <p>oneblackholemail@gmail.com</p>
+            </div>
 
-</html>
+            <script>
+                $('#logb').click(function() {
+                    $('#sign').css("display", "none");
+                    $('#cnt').css("display", "none");
+                    if ($(window).width() >= 1024) {
+                        $('#logbg').animate({
+                            top: "180vh",
+                        }, 500, function() {
+                            $('#log').css("display", "block");
+                        });
+
+                        $('#signbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#cnt').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+
+                    } else {
+                        $('#logbg').animate({
+                            top: "80vh",
+                        }, 500, function() {
+                            $('#log').css("display", "block");
+                        });
+                        $('#signbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#cnt').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                    }
+                });
+
+                $('#signb').click(function() {
+                    $('#log').css("display", "none");
+                    $('#cnt').css("display", "none");
+                    if ($(window).width() >= 1024) {
+                        $('#signbg').animate({
+                            top: "180vh",
+                        }, 500, function() {
+                            $('#sign').css("display", "block");
+                        });
+                        $('#logbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#cnt').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+
+                    } else {
+                        $('#signbg').animate({
+                            top: "80vh",
+                        }, 500, function() {
+                            $('#sign').css("display", "block");
+                        });
+                        $('#logbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#cnt').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                    }
+                });
+
+                $('#cntb').click(function() {
+                    $('#sign').css("display", "none");
+                    $('#log').css("display", "none");
+                    if ($(window).width() >= 1024) {
+                        $('#cnt').animate({
+                            top: "180vh",
+                        }, 500, function() {
+                            $('#cnt').css("display", "block");
+                        });
+                        $('#logbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#signbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+
+                    } else {
+                        $('#cnt').animate({
+                            top: "80vh",
+                        }, 500, function() {
+                            $('#cnt').css("display", "block");
+                        });
+                        $('#logbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                        $('#signbg').animate({
+                            top: "35vh",
+                        }, 250, function() {});
+                    }
+                });
+
+            </script>
+
+    </body>
+
+    </html>
