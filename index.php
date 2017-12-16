0513881fc4bda183f9b9eff9137bc0005395ba3e
Hugo Vale Pereira <hvpereira@gmail.com>
2017-12-16 01:19:10 +0000

    esta

--- index.php
+++ index.php
@@ -11,11 +11,12 @@ $nome = $email = $password = $password_confirm = $email_login = $password_login=
 <!doctype html>
 <html>
 <head>
-  <title>VYNIL STORE</title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
+        <title>VYNIL STORE</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
 <body>
   <?php
 
@@ -154,33 +155,139 @@ $nome = $email = $password = $password_confirm = $email_login = $password_login=
   ?>
 
   <h1>VYNIL STORE</h1>
+  <img src="img/vynil.png" id="vynil">
+  <div id="logb">
+    <h2>LOG IN</h2>
+  </div>
+  <div id="logbg"></div>
+  <div id="log">
+    <form method="post">
+      email: <input type="email" placeholder="email" name="email_login"><br><br> password: <input type="password" name="password_login"><br><br>
+      <input type="submit" name="submit_login" value="L O G I N"><br>
 
-  <h2>LOG IN</h2>
-  <form method="post" >
-    email: <input type="email" placeholder="email" name="email_login">
-    <span class="error"> <?php echo $email_loginErr;?></span><br>
-    password: <input type="password" name="password_login">
-    <span class="error"> <?php echo $password_loginErr;?></span><br>
-    <input type="submit" name="submit_login" value="L O G I N"><br>
+    </form>
+  </div>
 
-  </form>
+  <div id="signb">
+    <h2>SIGN UP</h2>
+  </div>
+  <div id="signbg"></div>
+  <div id="sign">
+    <form method="post">
+      nome: <input type='text' name="nome">
+      <span class="error"> <?php echo $nomeErr;?></span><br><br> email: <input type="email" name="email">
+      <span class="error"> <?php echo $emailErr;?></span><br><br> password: <input type="password" name="password">
+      <span class="error"> <?php echo $passwordErr;?></span><br> confirm password: <input type="password" name="password_confirm">
+      <span class="error"> <?php echo $password_confirmErr;?></span><br><br>
+      <input type="submit" name="submit" value="C O N F I R M"><br>
 
-  <h2>SIGN UP</h2>
-  <form method="post">
-    nome: <input type='text' name="nome">
-    <span class="error"> <?php echo $nomeErr;?></span><br>
-    email: <input type="email" name="email">
-    <span class="error"> <?php echo $emailErr;?></span><br>
-    password: <input type="password" name="password">
-    <span class="error"> <?php echo $passwordErr;?></span><br>
-    confirm password: <input type="password" name="password_confirm">
-    <span class="error"> <?php echo $password_confirmErr;?></span><br>
-    <input type="submit" name="submit" value="C O N F I R M"><br>
+    </form>
+  </div>
 
-  </form>
+  <div id="cntb">
+    <h2>CONTACT</h2>
+  </div>
+  <div id="cnt">
+    <p>oneblackholemail@gmail.com</p>
+  </div>
 
 
+  <script>
+  $('#logb').click(function() {
+    $('#sign').css("display", "none");
+    $('#cnt').css("display", "none");
+    if ($(window).width() >= 1024) {
+      $('#logbg').animate({
+        top: "180vh",
+      }, 500, function() {
+        $('#log').css("display", "block");
+      });
 
+      $('#signbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#cnt').animate({
+        top: "35vh",
+      }, 250, function() {});
+
+    } else {
+      $('#logbg').animate({
+        top: "80vh",
+      }, 500, function() {
+        $('#log').css("display", "block");
+      });
+      $('#signbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#cnt').animate({
+        top: "35vh",
+      }, 250, function() {});
+    }
+  });
+
+  $('#signb').click(function() {
+    $('#log').css("display", "none");
+    $('#cnt').css("display", "none");
+    if ($(window).width() >= 1024) {
+      $('#signbg').animate({
+        top: "180vh",
+      }, 500, function() {
+        $('#sign').css("display", "block");
+      });
+      $('#logbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#cnt').animate({
+        top: "35vh",
+      }, 250, function() {});
+
+    } else {
+      $('#signbg').animate({
+        top: "80vh",
+      }, 500, function() {
+        $('#sign').css("display", "block");
+      });
+      $('#logbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#cnt').animate({
+        top: "35vh",
+      }, 250, function() {});
+    }
+  });
+
+  $('#cntb').click(function() {
+    $('#sign').css("display", "none");
+    $('#log').css("display", "none");
+    if ($(window).width() >= 1024) {
+      $('#cnt').animate({
+        top: "180vh",
+      }, 500, function() {
+        $('#cnt').css("display", "block");
+      });
+      $('#logbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#signbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+
+    } else {
+      $('#cnt').animate({
+        top: "80vh",
+      }, 500, function() {
+        $('#cnt').css("display", "block");
+      });
+      $('#logbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+      $('#signbg').animate({
+        top: "35vh",
+      }, 250, function() {});
+    }
+  });
+
+  </script>
 </body>
 
 </html>
--- shop.php
+++ shop.php
@@ -13,9 +13,19 @@ $_SESSION['carro']=[];
 <head>
   <title>VYNIL STORE</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="shop.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+  <div class="sidebars" id="leftsb">
+    <div id="lb"><i class="fa fa-align-justify fa-4x" aria-hidden="true" id="l-icon"></i></div>
+  </div>
+
+  <div class="sidebars" id="rightsb">
+    <div id="rb"><i class="fa fa-user-circle fa-4x" aria-hidden="true" id="r-icon"></i></div>
+  </div>
   <h1>VYNIL STORE - SHOP</h1>
 
   <main class="main">
@@ -32,7 +42,29 @@ $_SESSION['carro']=[];
     }
     ?>
   </main>
+  <script>
+  $('#l-icon').click(function() {
+    $('#leftsb').animate({
+      width: "600px",
+    }, 500, function() {
+    });
+    $('#l-icon').animate({
+      left: "240px",
+    }, 500, function() {
+    });
+  });
 
+  $('#r-icon').click(function() {
+    $('#rightsb').animate({
+      width: "600px",
+    }, 500, function() {
+    });
+    $('#r-icon').animate({
+      right: "240px",
+    }, 500, function() {
+    });
+  });
+  </script>
 
 </body>
 
