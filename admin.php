945a7a51994f72a203169f7af33927ce59fd2870
Hugo Vale Pereira <hvpereira@gmail.com>
2017-12-16 01:18:11 +0000

    master funcional

--- admin.php
+++ admin.php
@@ -12,10 +12,19 @@ $album_ano=$album_duracao=$album_nMusicas=$album_stock=$album_preco="";
 <head>
   <title>VYNIL STORE</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="shop.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+  <div class="sidebars" id="leftsb">
+    <div id="lb"><i class="fa fa-align-justify fa-4x" aria-hidden="true" id="l-icon"></i></div>
+  </div>
+
+  <div class="sidebars" id="rightsb">
+    <div id="rb"><i class="fa fa-user-circle fa-4x" aria-hidden="true" id="r-icon"></i></div>
+  </div>
 
   <h1>VYNIL STORE - ÁREA DE ADMINISTRADOR</h1>
 
@@ -100,7 +109,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $("#adicionar").toggleClass( "hide" )
  });
 
-}
+
  </script>
 
 </body>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 
-
 <?php
 include 'func.php';
 
